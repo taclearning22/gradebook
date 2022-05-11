6f2726b10927bb13a2b594bd9043a0d30850a39a
parag gawde -X (pagawde - CAPGEMINI AMERICA INC at Cisco) <pagawde@cisco.com>
2022-05-11 20:57:53 +0530

    feat: Add code for module 1

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (args.Length > 0)
+{
+    Console.WriteLine($"Hello, {args[0]}!");
+}
+else 
+{
+    Console.WriteLine("Hello C#!");
+}
