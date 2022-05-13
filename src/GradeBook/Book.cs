38ebfa242c68caeef6410d2edd15e5b860a1e639
TAC <taclearning22@gmail.com>
2022-05-13 16:44:07 +0530

    feat: Add code for module 5

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -5,12 +5,12 @@ namespace GradeBook // Note: actual namespace depends on the project name.
     public class Book
     {
         private List<double> grades;
-        private string name;
+        public string Name;
 
         public Book(string name)
         {
             grades = new List<double>();
-            this.name = name;
+            Name = name;
         }
         public void AddGrade(double grade)
         {
